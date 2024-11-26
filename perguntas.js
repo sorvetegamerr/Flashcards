dfc67c5cc99689b087ad8b13c9eb37d0fdbab9e1
sorvetegamerr <00001116009870SP@al.educacao.sp.gov.br>
2024-11-25 22:55:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+criaCartao(
+   'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'História', 
+    'Quem descobriu o Brasil?',
+    "Pedro Alavares Cabral"
+)
+
+criaCartao(
+    'Programação', 
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua Inglesa', 
+    'Como se diz tchau em inglês?',
+    'Tchau em inglês é Bye'
+)
+
+
